74e26bac73aa39cd187aeee22464dc84517a4cf4
Leonidos1996 <l.pesk@yandex.ru>
2023-09-15 18:14:08 +0300

    Project HH

--- src/main/java/projectHHFromLeonid/tracker/MainServiceDomain.java
+++ src/main/java/projectHHFromLeonid/tracker/MainServiceDomain.java
@@ -1,9 +1,11 @@
 package projectHHFromLeonid.tracker;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainServiceDomain {
     public final RestTemplate restTemplate;
@@ -14,9 +16,15 @@ public class MainServiceDomain {
 
     public void downloadAndSaveVacancies(){
         RestTemplate restTemplate = new RestTemplate();
-
         MainServiceDomain mainServiceDomain = restTemplate.getForObject("https://api.hh.ru/vacancies", MainServiceDomain.class);
-        ArrayList<String> allVacancies = new ArrayList<>();
+      //  ArrayList<String> allVacancies = new ArrayList<>();
+
+        String fooResourceUrl
+                = "http://localhost:8080/spring-rest/foos";
+        ResponseEntity<List> response
+                = restTemplate.getForEntity(fooResourceUrl, List.class);
+        System.out.println(response);
+
 
 
     }
